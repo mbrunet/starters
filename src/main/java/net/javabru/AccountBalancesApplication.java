3717110edc2753ad4728b2e0560e2f270ccff85e
Michel Brunet <michel@suffix.javabru>
2016-08-26 21:55:13 -0400

    Inital commit.

---
+++ src/main/java/net/javabru/AccountBalancesApplication.java
@@ -0,0 +1,12 @@
+package net.javabru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountBalancesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountBalancesApplication.class, args);
+    }
+}
---
+++ src/test/java/net/javabru/AccountBalancesApplicationTests.java
@@ -0,0 +1,18 @@
+package net.javabru;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = AccountBalancesApplication.class)
+@WebAppConfiguration
+public class AccountBalancesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
